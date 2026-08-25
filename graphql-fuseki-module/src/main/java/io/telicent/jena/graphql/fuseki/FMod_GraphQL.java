01e30b33e4ed66251d9b0a93ac2db03cbe4bc113
Paul Gallagher <paul.gallagher@telicent.io>
2026-08-25 08:38:05 +0000

    [CORE-1473] Addressing SonarQube issues - suppressing java:S101

--- graphql-fuseki-module/src/main/java/io/telicent/jena/graphql/fuseki/FMod_GraphQL.java
+++ graphql-fuseki-module/src/main/java/io/telicent/jena/graphql/fuseki/FMod_GraphQL.java
@@ -35,6 +35,7 @@ import java.util.Set;
  * A Fuseki module that provides a GraphQL operation that can be used to accept GraphQL queries against the underlying
  * Dataset using an arbitrary schema as provided by the underlying {@link GraphQLOverDatasetExecutor} implementation.
  */
+@SuppressWarnings("java:S101")
 public class FMod_GraphQL implements FusekiAutoModule {
 
     private static final String VERSION = Version.versionForClass(FMod_GraphQL.class).orElse("<development>");
